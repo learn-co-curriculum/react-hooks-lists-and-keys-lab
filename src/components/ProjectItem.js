a434c9b62fe2cf40f88600241d544cc1d96df95b
Brendan Baird <brendan.b517@gmail.com>
2023-01-18 00:29:51 -0500

    commit tree c15cd798deb82ddf45ff323f0ae6dc2ec4b5ea01

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,7 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map(technology => <span key={technology}>{technology}</span>)}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,8 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map(project => <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />
+      )}</div>
     </div>
   );
 }
