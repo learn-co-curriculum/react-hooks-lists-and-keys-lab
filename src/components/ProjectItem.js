e54b8cdf6b2d7188ce35f536f833b09b001454b9
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-22 16:52:49 -0700

    added ProjectItem component

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,9 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => (
+          <span key = {technology}>{technology}</span>
+        ))}
       </div>
     </div>
   );
