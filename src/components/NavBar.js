fe356a420204df963e9f73b65650faca55e60941
Peter Ndirangu Nyambura <138131705+peternyambura@users.noreply.github.com>
2023-08-22 13:59:49 +0300

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link, index) => (
+    <a key={index} href={`#${link}`}>
+      {link}
+    </a>
+  ));
+
+  return <nav>{linkElements}</nav>;
 }
 
 export default NavBar;
